17743a8fa3bc0995c18f0f85ec5e42885f1b0d28
yym0807 <75062543+yym0807@users.noreply.github.com>
2024-05-29 18:13:09 +0800

    for debug

--- final_project/all.py
+++ final_project/all.py
@@ -417,6 +417,8 @@ while True:
     now_lat=newmsg.latitude
     now_lng=newmsg.longitude
     if float(now_lat) > 26 or float(now_lat) < 21 or float(now_lng) > 123 or float(now_lng) < 119:
+        print(now_lat)
+        print(now_lng)
         continue
     #geolocator = Nominatim(user_agent="gps")
     #location = geolocator.reverse(str(now_lat)+","+str(now_lng))
